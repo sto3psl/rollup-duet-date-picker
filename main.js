3e0bcfb02b69f9f95c7b6ebebc1c00d3cdf94386
Fabian Gündel <5798652+sto3psl@users.noreply.github.com>
2020-09-25 21:20:51 +0200

    fix with plugin

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import "@duetds/date-picker/dist/duet/themes/default.css";
 import { defineCustomElements } from "@duetds/date-picker/dist/esm/loader";
 
 defineCustomElements(window);
--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,31 @@
 import resolve from "@rollup/plugin-node-resolve";
+import css from "rollup-plugin-css-only";
 
 export default {
   input: "main.js",
   // Fix Warning: 	(!) `this` has been rewritten to `undefined`
   // 				https://rollupjs.org/guide/en/#error-this-is-undefined
   context: "window",
+  preserveEntrySignatures: false,
   output: {
     dir: "public",
     format: "esm",
   },
-  plugins: [resolve()],
+  plugins: [
+    {
+      resolveDynamicImport(specifier, importer) {
+        const matches = !!importer.match(
+          /@duetds\/date-picker\/dist.*\/index-/g
+        );
+
+        if (typeof specifier === "string" || !matches) return null;
+
+        return this.resolve(
+          "@duetds/date-picker/dist/esm/duet-date-picker.entry.js"
+        );
+      },
+    },
+    css({ output: "public/main.css" }),
+    resolve(),
+  ],
 };
